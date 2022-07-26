cee4228525a54d8f4320b7aa3b1709b2c8a801b1
Md yasen rafe <mdyasenrafe@gmail.com>
2022-07-26 15:00:36 +0600

    filter products bug resolve

--- src/Controllers/ProductController.js
+++ src/Controllers/ProductController.js
@@ -1,34 +1,8 @@
 const ProductsModel = require("../Models/ProductModel");
 const axios = require("axios");
 
-exports.AddProduct = async (req, res) => {
-  ProductsModel.create(req.body, (err, data) => {
-    if (err) {
-      res.status(200).json({ error: true, message: err });
-    } else {
-      res.status(200).json({
-        error: false,
-        message: "created successfully",
-        data: data,
-      });
-    }
-  });
-};
-exports.getProducts = async (req, res) => {
-  ProductsModelNew.find({}, (err, data) => {
-    if (err) {
-      res.status(200).json({ error: true, message: err });
-    } else {
-      res.status(200).json({
-        error: false,
-        message: "data fetch successfully",
-        data: data,
-      });
-    }
-  });
-};
-exports.filterProducts = async (req, res) => {
-  ProductsModel.find({ filter: req.body.filter }, (err, data) => {
+const allProducts = async (req, res) => {
+  ProductsModel.find({}, (err, data) => {
     if (err) {
       res.status(200).json({ error: true, message: err });
     } else {
@@ -40,4 +14,29 @@ exports.filterProducts = async (req, res) => {
       });
     }
   });
+};
+
+exports.AddProduct = async (req, res) => {
+  allProducts(req, res);
+};
+
+exports.getProducts = async (req, res) => {};
+exports.filterProducts = async (req, res) => {
+  const filter = req.body.filter;
+  if (!filter) {
+    allProducts(req, res);
+  } else {
+    ProductsModel.find({ filter: filter }, (err, data) => {
+      if (err) {
+        res.status(200).json({ error: true, message: err });
+      } else {
+        res.status(200).json({
+          error: false,
+          message: "data fetch successfully",
+          count: data.length,
+          data: data,
+        });
+      }
+    });
+  }
 };
