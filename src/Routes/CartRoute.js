f12357888132218b71ebf654156055f847b27113
Md yasen rafe <mdyasenrafe@gmail.com>
2022-07-27 12:17:50 +0600

    update cart added

--- src/Routes/CartRoute.js
+++ src/Routes/CartRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { AddCart, GetCart } = require("../Controllers/CartController");
+const {
+  AddCart,
+  GetCart,
+  updateCart,
+} = require("../Controllers/CartController");
 
 const router = express.Router();
 
 router.post("/addCart", AddCart);
 router.post("/getCart", GetCart);
+router.post("/update", updateCart);
 
 module.exports = router;
